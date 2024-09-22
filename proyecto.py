062b4e0262e6b49e5eb4962638e1c2f1bbb2d824
Alexis Viglianti <148451612+Andext@users.noreply.github.com>
2024-09-22 05:16:54 -0300

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,19 +1,41 @@
 import py5
 
-x = 0
+x, y = 0, 0
+x_speed, y_speed = 2, 2
+logo = None
 
 def setup():
-    py5.size(400, 400)
-    py5.background(255)
+    global logo, x, y
+    py5.size(800, 600) 
+    logo = py5.load_image('/mnt/data/image.png') 
+    x = py5.width / 2  
+    y = py5.height / 2
 
 def draw():
-    global x
-    py5.background(255)
-    py5.fill(150, 0, 150)
-    py5.circle(x, 200, 50)
-    x += 2 # Mueve el círculo hacia la derecha
+    global x, y, x_speed, y_speed
+    py5.background(0) 
 
-    if x > py5.width:
-        x = 0 # Reinicia la posición si se sale de la ventana
+   
+    py5.image(logo, x, y)
+
+ 
+    x += x_speed
+    y += y_speed
+
+    
+    if x <= 0 or x + logo.width >= py5.width:
+        x_speed *= -1
+    if y <= 0 or y + logo.height >= py5.height:
+        y_speed *= -1
+
+def key_pressed():
+    global x_speed, y_speed
+  
+    if py5.key == py5.UP:
+        x_speed += 1
+        y_speed += 1
+    elif py5.key == py5.DOWN:
+        x_speed -= 1
+        y_speed -= 1
 
 py5.run_sketch()
